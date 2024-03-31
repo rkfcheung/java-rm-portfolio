6a4b37fcbd5063331119eb4bf404e0065b1b1489
Raymond Cheung <korekontakt@gmail.com>
2024-03-31 19:15:22 +0100

    Add H2 database

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,14 @@ repositories {
 
 dependencies {
     val cucumberVersion = "7.16.1"
+    val hibernateOrmVersion = "6.4.4.Final"
+    val sqliteVersion = "3.45.2.0"
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 //	implementation("org.liquibase:liquibase-core")
+    implementation("com.h2database:h2")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
